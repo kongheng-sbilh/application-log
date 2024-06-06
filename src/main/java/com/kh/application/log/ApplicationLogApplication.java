b23fef539ab75e02e2d6183c978784f70e2f69d5
kongheng-sbilh <kongheng.long@sbilhbank.com.kh>
2024-06-06 15:28:42 +0700

    feat: init project

---
+++ src/main/java/com/kh/application/log/ApplicationLogApplication.java
@@ -0,0 +1,13 @@
+package com.kh.application.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/application/log/ApplicationLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.application.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
